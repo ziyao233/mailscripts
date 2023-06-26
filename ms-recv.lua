8fda75d85bf2663ed5c31e70775c30599b05eaa1
Yao Zi <ziyao@disroot.org>
2023-06-27 01:27:22 +0800

    Fix ms-recv fails on rare messages

--- ms-recv.lua
+++ ms-recv.lua
@@ -11,9 +11,9 @@
 conf	= assert(loadfile(os.getenv("HOME") .. "/.msrc.lua"))();
 pop3	= conf.pop3;
 
-user	= ("%s:%s"):format(pop3.user,pop3.password);
-url	= ("%s://%s"):format(pop3.tls and "pop3" or "pop3s",pop3.server);
-pipe	= io.popen(("curl -s --user %s %s"):format(user,url),"r");
+user	= ("%s:%s"):format(pop3.user, pop3.password);
+url	= ("%s://%s"):format(pop3.tls and "pop3" or "pop3s", pop3.server);
+pipe	= io.popen(("curl -s --user %s %s"):format(user,url), "r");
 output = pipe:read("a");
 count = 0;
 for _ in string.gmatch(output,"\n")
@@ -25,24 +25,26 @@ print("Message Number: " .. count);
 pipe:close();
 
 getFrom = function(mail)
-	local id = string.match(mail,[[From:%s*(.->)]]);
-	return id,string.match(id,[[<(.-)>]]);
+	local id = string.match(mail, [[From:%s*(.->)]]);
+	return id,string.match(id, [[<(.-)>]]);
 end
 getDate = function(mail)
-	local date = string.match(mail,"Date:%s(.-)[%+%-]");
-	return string.match(date,"%d%d %a%a%a %d%d%d%d %d%d:%d%d:%d%d");
+	local date = string.match(mail, "Date:%s*([^\n]-)[%+%-]%d%d%d%d");
+	return string.match(date, "%d+ %a%a%a %d%d%d%d %d+:%d+:%d+");
 end
 getMail = function()
-	local pipe = io.popen(("curl -s --user %s %s/1"):format(user,url),"r");
+	local pipe = io.popen(("curl -s --user %s %s/1"):format(user, url),"r");
 	local mail = pipe:read("a");
 	pipe:close();
+	io.stderr:write(mail .. "\n\n\n");
 	return mail;
 end
 strip = function(s)
-	return string.gsub(s,"%W","_");
+	return string.gsub(s, "%W", "_");
 end
 deleteMail = function()
-	os.execute(("curl --request DELE -l -s --user %s %s/1"):format(user,url));
+	os.execute(("curl --request DELE -l -s --user %s %s/1"):
+		   format(user, url));
 	return;
 end
 
@@ -53,7 +55,7 @@ do
 	local date		= getDate(mail);
 	print(addr .. ":\t" .. date);
 	local file		= io.open(strip(addr) .. "_" ..
-					  strip(date),"w");
+					  strip(date) .. ".mail", "w");
 	file:write(mail);
 	file:close();
 	deleteMail();
