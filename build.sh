e29b66e8cc4d8a307358e76792f07e1a17eef5ef
Yao Zi <ziyao@disroot.org>
2023-06-26 23:06:10 +0800

    Add build.sh
    - Create ms-common.lua for shared code

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#	mailscripts
+#	build.sh
+#	By Mozilla Public License Version 2.0
+#	Copyright (c) 2023 Yao Zi. All rights reserved.
+
+# This script build scripts with lmerge
+
+commonModule=ms-common.lua
+scripts="ms-recv ms-stat ms-send"
+
+mkdir -p ./build
+
+for script in $scripts
+do
+	echo Building $script...
+	lmerge -ishb -o build/$script -m $script.lua $script.lua ms-common.lua
+	chmod 755 build/$script
+done
+
+echo Building done
---
+++ ms-common.lua
@@ -0,0 +1,13 @@
+--[[
+--	mailscripts
+--	ms-common.lua
+--	By Mozilla Public License Version 2.0
+--	Copyright (c) 2023 Yao Zi. All rights reserved.
+--]]
+
+--[[
+--	This file contains some commonm functions used by different scripts.
+--]]
+
+return {
+       }
