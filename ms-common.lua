f561eea23d965f1bdbe6a87a602098e01e74ec69
Yao Zi <ziyao@disroot.org>
2023-06-27 02:42:09 +0800

    Refact ms-recv.lua
    - Move code that could be commonly used to ms-common.lua

--- ms-common.lua
+++ ms-common.lua
@@ -9,5 +9,33 @@
 --	This file contains some commonm functions used by different scripts.
 --]]
 
+local function
+getFrom(mail)
+	local id = string.match(mail, [[From:%s*(.->)]]);
+	return id,string.match(id, [[<(.-)>]]);
+end
+
+local function
+getDate(mail)
+	local date = string.match(mail, "Date:%s*([^\n]-)[%+%-]%d%d%d%d");
+	return string.match(date, "%d+ %a%a%a %d%d%d%d %d+:%d+:%d+");
+end
+
+local function
+strip(s)
+	return string.gsub(s, "%W", "_");
+end
+
+local function
+filename(mail)
+	local _, addr	= getFrom(mail);
+	local date	= getDate(mail);
+	return ("%s_%s.mail"):format(strip(addr), strip(date));
+end
+
 return {
+	getFrom		= getFrom,
+	getDate		= getDate,
+	strip		= strip,
+	filename	= filename,
        }
--- ms-recv.lua
+++ ms-recv.lua
@@ -8,6 +8,8 @@
 --	Copyright (c) 2022-2023 Ziyao.
 --]]
 
+local ms		= require "ms-common"
+
 conf	= assert(loadfile(os.getenv("HOME") .. "/.msrc.lua"))();
 pop3	= conf.pop3;
 
@@ -24,38 +26,26 @@ end
 print("Message Number: " .. count);
 pipe:close();
 
-getFrom = function(mail)
-	local id = string.match(mail, [[From:%s*(.->)]]);
-	return id,string.match(id, [[<(.-)>]]);
-end
-getDate = function(mail)
-	local date = string.match(mail, "Date:%s*([^\n]-)[%+%-]%d%d%d%d");
-	return string.match(date, "%d+ %a%a%a %d%d%d%d %d+:%d+:%d+");
-end
 getMail = function()
 	local pipe = io.popen(("curl -s --user %s %s/1"):format(user, url),"r");
 	local mail = pipe:read("a");
 	pipe:close();
-	io.stderr:write(mail .. "\n\n\n");
 	return mail;
 end
-strip = function(s)
-	return string.gsub(s, "%W", "_");
-end
+
 deleteMail = function()
 	os.execute(("curl --request DELE -l -s --user %s %s/1"):
 		   format(user, url));
 	return;
 end
 
-for i = 1,count
+for i = 1, count
 do
 	local mail		= getMail();
-	local id,addr		= getFrom(mail);
-	local date		= getDate(mail);
+	local id,addr		= ms.getFrom(mail);
+	local date		= ms.getDate(mail);
 	print(addr .. ":\t" .. date);
-	local file		= io.open(strip(addr) .. "_" ..
-					  strip(date) .. ".mail", "w");
+	local file		= io.open(ms.filename(mail), "w");
 	file:write(mail);
 	file:close();
 	deleteMail();
